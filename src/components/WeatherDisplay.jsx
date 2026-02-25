987da23d1aac2ac19dc67e7324b8f186c771823f
Udit Kumar <kudit223@gmail.com>
2026-02-25 15:42:03 +0530

    updated

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Weather({temperature,conditions}){
+export default function WeatherDisplay({temperature,conditions}){
     let tempColor=temperature>20?'red':'blue';
     return(<>
     <p><span style={{color:tempColor}}> temperature:{temperature}</span> conditions:{conditions}</p>
